74d4c21ffdf71e0855142640edc83ea7ea2272e5
vinnayak2024 <vinayaksaxena20@gmail.com>
2026-03-03 19:16:26 +0530

    Usecase2 completed

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,15 +1,37 @@
 public class PalindromeChecker {
-    public static class UseCase1{
-        public UseCase1(){
-            System.out.println("Welcome to the Palindrome Checker Management System");
-            System.out.println("Version: 1.0");
-            System.out.println("System initalised successfully");
+    public static class UseCase2{
+        public UseCase2(String s){
+            s=s.toLowerCase();
+            boolean isPlaindrome;
+            int start= 0;
+            int end = s.length() -1;
+            while(start<end){
+                if(s.charAt(start) != s.charAt(end)){
+                    isPlaindrome=false;
+                }
+                start++;
+                end--;
+            }
+
+            isPlaindrome=true;
+
+            if(isPlaindrome){
+                System.out.println("Is it a Palindrome:Yes");
+            }
+            else{
+                System.out.println("Is it a Palindrome:No");
+            }
+
         }
 
 
     }
     public static void main(String[] args){
-        UseCase1 entry = new UseCase1();
+        String s = "madam";
+        System.out.println(("Input Text: madam"));
+        UseCase2 entry = new UseCase2(s);
+
+
 
     }
 }
