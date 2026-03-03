1498b4293deb2786dbad0dbe3acbd498a292a462
vinnayak2024 <vinayaksaxena20@gmail.com>
2026-03-03 20:59:00 +0530

    USECASE 10 COMPLETED

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,4 +1,4 @@
-public class UseCase9PalindromeCheckerApp {
+public class UseCase10PalindromeCheckerApp {
 
     static class Recursion {
 
@@ -19,9 +19,13 @@ public class UseCase9PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String s = "bob";
+        String s = "A Man a Plan a Canal Panama";
+        String newString = s.toLowerCase();
+        System.out.println(newString);
+        String removedspace = newString.replace(" ","");
+        System.out.println(removedspace);
 
-        if (Recursion.Palindrome(s, 0, s.length() - 1))
+        if (Recursion.Palindrome(removedspace, 0, removedspace.length() - 1))
             System.out.println("Palindrome");
         else
             System.out.println("Not palindrome");
