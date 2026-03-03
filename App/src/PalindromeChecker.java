384ec11557597d5a9f510b474b7a5f19d3dca490
vinnayak2024 <vinayaksaxena20@gmail.com>
2026-03-03 19:10:03 +0530

    usecase 1 completed

---
+++ App/src/PalindromeChecker.java
@@ -0,0 +1,15 @@
+public class PalindromeChecker {
+    public static class UseCase1{
+        public UseCase1(){
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version: 1.0");
+            System.out.println("System initalised successfully");
+        }
+
+
+    }
+    public static void main(String[] args){
+        UseCase1 entry = new UseCase1();
+
+    }
+}
